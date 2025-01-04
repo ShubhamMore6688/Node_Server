814dbf00edf1aca00686c75b9186cd2f09b3604e
ShubhamMore6688 <shubhamdmore03@gmail.com>
2025-01-04 14:28:18 +0530

    simple nodejs server

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+// express server
+const app = express();
+
+// route
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    })
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Server is up and running on port 3000');
+});
